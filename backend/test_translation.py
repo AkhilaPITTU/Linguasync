23b26daeee0f22e0652fb25e046e0b38a9f85d4c
AkhilaPittu <pittuakhilareddy@gmail.com>
2026-08-29 00:18:32 +0530

    Sync latest project changes

--- backend/test_translation.py
+++
@@ -1,9 +0,0 @@
-from app.ai.translation_service import translation_service
-
-result = translation_service.translate(
-    "Welcome to LinguaSync",
-    source_lang="en",
-    target_lang="es"
-)
-
-print(result)
